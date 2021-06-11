dd051202ec31c0e5fd56bb91c7bcf7aa248959f2
sunilghimire <sunilghimire64@gmail.com>
2021-06-11 08:25:45 +0545

    Program - Array

--- Array/array.py
+++ Array/array.py
@@ -1,21 +1,28 @@
 from array import *
 
-arr =  array('i', [])
+arr = array('i', [])
 
 length = int(input('Enter the length of the array: '))
 
 for i in range(length):
-    values = int(input('Enter the next value: '))
+    values = int(input('Enter the value you want: '))
     arr.append(values)
 
 print(arr)
 
-search_value = int(input('Enter the value for search: '))
+print('The size of an array is', len(arr))
+
+search_values = int(input('Enter the value you want to search: '))
 
 count = 0
-for a in arr:
-    if a == values:
-        print(count)
-        break
-    count += 1
 
+for a in arr:
+    if a == search_values:
+        print('The index value of element {} is {}.'.format(search_values, count + 1 ))
+        break
+    count += 1 
+    
+    print('Sorry, Index value not found')
+    break
+    
+   
