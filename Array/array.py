74a7e82d3da33a28087bb41966c701cfd90a5ad4
sunilghimire <sunilghimire64@gmail.com>
2021-06-11 18:39:04 +0545

    Program: Array

--- Array/array.py
+++ Array/array.py
@@ -8,21 +8,29 @@ for i in range(length):
     values = int(input('Enter the value you want: '))
     arr.append(values)
 
-print(arr)
+print("The original array coming from user's input: {} ".format(arr))
 
-print('The size of an array is', len(arr))
+print('The size of an array is {}. \n'.format(len(arr)))
+
+## removing duplicates element from an array 
+res = array('i', [])
+for dup in arr:
+    if dup not in res:
+        res.append(dup)
+print('Element after removing duplicate values(if exist) : {} \n'.format(res))
 
 search_values = int(input('Enter the value you want to search: '))
 
 count = 0
 
-for a in arr:
+## Searching the index of an element
+for a in res:
     if a == search_values:
-        print('The index value of element {} is {}.'.format(search_values, count + 1 ))
+        print('The index value of element {} is at {}.'.format(search_values, count + 1 ))
         break
     count += 1 
     
-    print('Sorry, Index value not found')
-    break
+else:
+    print('Sorry, Index value not found.')
+
     
-   
